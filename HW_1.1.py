951ee1808e2e5bbffcd2fcf87934d3215bb11a6e
Lyubomyr Ilynych <ilynych@gmail.com>
2019-11-05 09:47:35 +0200

    add new file :HW_1.1 and HW_1.2

---
+++ HW_1.1.py
@@ -0,0 +1,7 @@
+#task1
+a = int(input("enter first number:"))
+b= int(input("enter second number:"))
+print("a+b=",a+b)
+print("a-b=",a-b)
+print("a*b=",a*b)
+print("a/b=",a/b)
---
+++ HW_1.2.py
@@ -0,0 +1,7 @@
+#task2
+name = input("What is your name?")
+year = input("How old are you?")
+home = input("Where do you live?")
+print("name -\n"+" "+ name  )
+print("year -\n "+" "+year)
+print("city - \n"+" "+home)
